1155341950af8699631a4b82854a554310cdc211
Ineptus <kuba.radzimowski@gmail.com>
2014-10-05 21:07:53 +0200

    upload

---
+++ Towers/src/com/ineptus/towers/Animation.java
@@ -0,0 +1,24 @@
+package com.ineptus.towers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Animation extends View {
+
+	public Animation(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Animation(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Animation(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+}
